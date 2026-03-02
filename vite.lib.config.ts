9d46268e9431478950456d694a066013fe6c896f
kazuri1 <96699295+kazuri1@users.noreply.github.com>
2026-03-02 21:35:59 +0530

    Fixed css issues

--- vite.lib.config.ts
+++ vite.lib.config.ts
@@ -1,10 +1,9 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 import { resolve } from "path";
-import { libInjectCss } from 'vite-plugin-lib-inject-css';
 
 export default defineConfig({
-  plugins: [react(), libInjectCss()],
+  plugins: [react()],
   build: {
     lib: {
       entry: resolve(__dirname, "src/index.ts"),
